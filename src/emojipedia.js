0b0e0166fecf3dc871df3e7821970d6fd5658ba0
Krzysztof Marciniak <109152045+kriszuz@users.noreply.github.com>
2022-09-21 13:26:29 +0200

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,116 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😆",
+    name: "Smiling Face With Open Mouth And Closed Eyes",
+    meaning:
+      "Emoji is on the verge of a fit of laughter because something is so silly or incredibly funny. Also, mischievous or bitchy laughter about an event, or something that happened to another person.."
+  },
+  {
+    id: 5,
+    emoji: "😁",
+    name: "Grinning Face With Smiling Eyes",
+    meaning:
+      "Happy face with a mischievous laugh. Represents lightheartedness and exuberance. Full of joy due to an event, excited and agitated or just a bit embarrassed."
+  },
+  {
+    id: 6,
+    emoji: "😋",
+    name: "Face Savouring Delicious Food",
+    meaning:
+      "Bon appetit! Smiley is licking the corner of its mouth with its tongue. Is hungry, just cooked something very delicious or eaten a whole dish. Often interpreted as a grimace, with tongue outstretched, and used after a funny message or when fooling around."
+  },
+  {
+    id: 7,
+    emoji: "😛",
+    name: "Face With Stuck-Out Tongue",
+    meaning:
+      "Horseplay with friends, weakening an ironic remark and making sure that the other does not take seriously what has been said. Making fun of others: I knew you'd screw it up."
+  },
+  {
+    id: 8,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning:
+      "I'm the king of the world! Mr. Cool Smiley with sunglasses. Is totally self-assured and relaxed. Everything is absolutely easy and cool. Also a symbol for sun, summer holidays, and holidays on the beach."
+  },
+  {
+    id: 9,
+    emoji: "🥳",
+    name: "Face with party blower and party hat",
+    meaning:
+      "That was a wild party or “Let's celebrate“. No matter what joyful event is in view, it will be celebrated appropriately. May also stand for the carnival season."
+  },
+  {
+    id: 10,
+    emoji: "😏",
+    name: "Smirking/Whimsical Face",
+    meaning:
+      "Half a smile that oozes with complacency. Represents irony, coolness or playfulness. Be careful: Somebody could be up to something. Used as an offensive look for flirting or sexual innuendo."
+  },
+  {
+    id: 11,
+    emoji: "😫",
+    name: "Tired Face",
+    meaning:
+      "Overtired emoji with narrowed eyes and open mouth. Because of too little sleep, because of what is going on around you, from a person or situation. Is very exhausted and broken. Now needs peace first."
+  },
+  {
+    id: 12,
+    emoji: "😳",
+    name: "Flushed Face",
+    meaning:
+      "Person is shocked, in an uncomfortable situation or has made a mistake. Expresses shame; something is very embarrassing. Reacts to a compliment or a flattering message that the person did not want to receive."
+  },
+  {
+    id: 13,
+    emoji: "🤯",
+    name: "Face With Exploding Head",
+    meaning:
+      "I cannot believe it. or “That will blow you away.“ Something is very exciting or surprising. The exploding head can also be used to express shock or awe."
+  },
+  {
+    id: 14,
+    emoji: "😱",
+    name: "Face Screaming in Fear",
+    meaning:
+      "Horror-stricken face with two hands, screaming. Is scared stiff, feeling nothing but fear and panic. The creepy emoji can also be used ironically or as a symbol for Halloween."
+  },
+  {
+    id: 15,
+    emoji: "😮",
+    name: "Face With Open Mouth",
+    meaning:
+      "Wow, I'm impressed! Perplexed smiley is looking completely puzzled. Is positively or negatively surprised: from mildly astonished to completely steamrolled."
+  },
+  {
+    id: 16,
+    emoji: "😯",
+    name: "Speechless Face",
+    meaning:
+      "Oh my goodness! The unpleasantly surprised face is lost for words due to a shocking affair. In response to bad behavior or a rude message. Nothing can be added to what has just been said."
+  }
+];
+
+export default emojipedia;
